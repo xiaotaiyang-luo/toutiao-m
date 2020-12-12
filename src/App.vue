a26b85173323ae24ebbc5556fcca0a90f0e5b70a
luoxiaobo <1355548741@qq.com>
2020-12-12 11:05:18 +0800

    项目初始化--引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <i class="toutiao toutiao-dianzan2"></i>
+    <van-button type="primary">主要按钮</van-button>
     <!-- 路由占位符 -->
     <router-view />
   </div>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入Vant 核心组件
+import Vant from 'vant';
+// 导入 Vant 全局样式
+import 'vant/lib/index.css';
+
+
+import '@/components/css/index.css'
+
 Vue.config.productionTip = false
 
+// 注册使用 Vant 组件库
+Vue.use(Vant);
 new Vue({
   router,
   store,
